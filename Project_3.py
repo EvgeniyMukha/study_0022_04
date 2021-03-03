e783cb908a3a8928fff68465fa45ce937bcb833c
EvgeniyMukha <78964967+EvgeniyMukha@users.noreply.github.com>
2021-03-03 16:44:57 +0300

    Add files via upload

---
+++ Project_3.py
@@ -0,0 +1,76 @@
+import math
+import random
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n*factorial(n-1)
+
+point = True
+print("/ деление \n* умножение \n+ сложение \n- разность\nm модуль\narccos - arccos \n^  возведение в степень \n! факториал \nrand рандомное число \nquit выход")
+
+while(point):
+    str = input()
+
+    if str == "/":
+        print("number =")
+        number = float(input())
+        print("second number = ")
+        secondnumber = float(input())
+        print(number / secondnumber)
+
+    elif str == "quit":
+        point = False
+
+    elif str == "*":
+        print("number =")
+        number = float(input())
+        print("second number = ")
+        secondnumber = float(input())
+        print(number * secondnumber)
+
+    elif str == "-":
+        print("number =")
+        number = float(input())
+        print("second number = ")
+        secondnumber = float(input())
+        print(number - secondnumber)
+
+    elif str == "+":
+        print("number =")
+        number = float(input())
+        print("second number = ")
+        secondnumber = float(input())
+        print(number + secondnumber)
+
+    elif str == "^":
+        print("number =")
+        number = float(input())
+        print("second number = ")
+        secondnumber = float(input())
+        print(number ** secondnumber)
+
+    elif str == "m":
+        print("number =")
+        number = float(input())
+        print(math.fabs(number))
+
+    elif str == "arccos":
+        print("number =")
+        number = float(input())
+        print(math.acos(number))
+
+    elif str == "!":
+        print("number =")
+        number = float(input())
+        print(factorial(number))
+
+    elif str == "rand":
+        print("Рандомное число в диапазона от: ")
+        number = int(input())
+        print("До :")
+        secondnumber = int(input())
+        print(random.randint(number, secondnumber))
+
+    else:
+        print("Команда отсутствует")
