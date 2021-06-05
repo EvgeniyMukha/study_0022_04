10b4ab4030a516de6c29e0df21903885f427bb96
Evgeniy Mukha <evgenii228068@mail.ru>
2021-06-06 00:02:55 +0300

    commit 1

--- Project_1.py
+++ Project_1.py
@@ -3,10 +3,11 @@ def main():
     numbborder = int(input('Введите пограничное число'))
     if numb < numbborder:
         print('Число меньше погрничного числа')
-    else:
-        print('Число больше пограничного числа')
-    if numb > numbborder * 3:
-        print('Число больше более чем в 3 раза чем пограничное число')
+    if numb > numbborder:
+        if numb > numbborder * 3:
+            print('Число больше более чем в 3 раза чем пограничное число')
+        else:
+            print('Число больше пограничного числа')
     if numb == numbborder:
         print('Числа равны')
 main()
