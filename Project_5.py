829ff774ab68b1ca74e6ebb63e1d04bfb876afc2
Evgeniy Mukha <78964967+EvgeniyMukha@users.noreply.github.com>
2021-06-08 19:05:10 +0300

    Update Project_5.py

--- Project_5.py
+++ Project_5.py
@@ -2,19 +2,19 @@ list_1 = [] # список 1
 list_2 = [] # список 2
 print("Для выхода из цикла введите - quit")
 while(True):
-    c = input()
-    if c == "quit":
+    word = input()
+    if word == "quit":
         break
     else:
         list_1.append(c) # append - вставляет в конце списка значение аргумента
-multipl = set(list_1) # делаем из массива множество
+multiplicity = set(list_1) # делаем из массива множество
 print("Список слов -",list_1)
-print("Множество - ",multipl)
+print("Множество - ",multiplicity)
 print("Количество слов в списке =",len(list_1))
 print("Заполните следующий список из",len(list_1),"слов")
 for i in range(len(list_1)):
-    c = input()
-    list_2.append(c)
+    word = input()
+    list_2.append(word)
 print(list_2)
 dictionary = {list_1[i]:list_2[i] for i in range(len(list_1))} # формируем словарь
 print(dictionary)
