84b86d4d40c5cd705685e559581cbc2e52f4b3bc
Evgeniy Mukha <78964967+EvgeniyMukha@users.noreply.github.com>
2021-06-08 19:18:10 +0300

    Update Project_7.py

--- Project_7.py
+++ Project_7.py
@@ -1,23 +1,21 @@
 def writing_file(name):
-    mass = []
+    array = []
 
-    r = 0
-
-    file = open(name + ".txt", "r")
+    file = open(name + ".txt", 'r')
 
     # заполняем массив строками
     for line in file:
-        mass.append(line)
+        array.append(line)
     file.close
-    file = open(name + ".txt", "w")
+    file = open(name + ".txt", 'w')
 
     for key, val in dictionary.items():
         file.write('{}:{}\n'.format(key, val))
     file.close
-    file = open(name + ".txt", "a")
+    file = open(name + ".txt", 'a')
 
-    for i in range(len(mass)):
-        file.write(mass[i])
+    for i in range(len(array)):
+        file.write(array[i])
     file.close
 
 
@@ -27,11 +25,11 @@ list_2 = []
 print("Для выхода из цикла введите - quit")
 
 while (True):
-    c = input()
-    if c == "quit":
+    word = input()
+    if word == "quit":
         break
     else:
-        list_1.append(c)
+        list_1.append(word)
 
 set_2 = set(list_1)
 print("список -", list_1)
@@ -40,8 +38,8 @@ print("количество символов в cgbcrt =", len(list_1))
 print("заполните следующий список из", len(list_1), "символов")
 
 for i in range(len(list_1)):
-    c = input()
-    list_2.append(c)
+    word = input()
+    list_2.append(word)
 
 print(list_2)
 complex = {list_2[i]: list_1[i] for i in range(len(list_1))}
